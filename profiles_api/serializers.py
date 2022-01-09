fc5fffa627ca2c37f0f1c50cf3f434f9f7b3a306
Emilzon <emilzonujero@gmail.com>
2022-01-08 21:45:55 -0600

    Added user profile feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -4,11 +4,11 @@ from profiles_api import models
 
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing our APIView."""
-    name = serializers.CharField(max_length=10) 
-    
+    name = serializers.CharField(max_length=10)
+
 class UserProfileSerializer(serializers.ModelSerializer):
     """Serializes a user profile object"""
-    
+
     class Meta:
         model = models.UserProfile
         fields = ('id', 'email', 'name', 'password')
@@ -18,7 +18,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
                 'style': {'input_type': 'password'}
             }
         }
-        
+
     def create(self, validated_data):
         """Create and return a new user"""
         user = models.UserProfile.objects.create_user(
@@ -26,13 +26,21 @@ class UserProfileSerializer(serializers.ModelSerializer):
             name = validated_data['name'],
             password = validated_data['password']
         )
-        
+
         return user
-    
+
     def update(self, instance, validated_data):
         """Handle updating user account"""
         if 'password' in validated_data:
             password = validated_data.pop('password')
             instance.set_password(password)
-        return super().update(instance, validated_data)    
-        
+        return super().update(instance, validated_data)
+
+class ProfieFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id','user_profile','status_text','created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
+
